044792e5759d1edc5a2f96fd02519172dc08295a
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-07-20 22:46:59 +0300

    feat: complete task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,80 @@
 class TicTacToe {
     constructor() {
-
+        this.currentPlayer = "x";
+        this.matrix = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ];
+        this.win = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+        ];
+        this.result = null;
+        this.winner = null;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (
+            this.result === null &&
+            this.matrix[rowIndex][columnIndex] === null
+        ) {
+            this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+            this.win.forEach((combo) => {
+                if (
+                    combo.every(
+                        (ind) => this.matrix.flat()[ind] === this.currentPlayer
+                    )
+                ) {
+                    this.result = this.currentPlayer;
+                    this.winner = this.currentPlayer;
+                }
+            });
+            if (this.noMoreTurns() && this.result == null) {
+                this.result = "draw";
+            }
+            this.currentPlayer === "x"
+                ? (this.currentPlayer = "o")
+                : (this.currentPlayer = "x");
 
+            return true;
+        } else {
+            return false;
+        }
     }
 
     isFinished() {
-
+        return this.result !== null;
     }
 
     getWinner() {
-
+        if (this.result === "draw") {
+            return null;
+        } else {
+            return this.result;
+        }
     }
 
     noMoreTurns() {
-
+        return !this.matrix.flat().includes(null);
     }
 
     isDraw() {
-
+        return this.result === "draw";
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex] || null;
     }
 }
 
